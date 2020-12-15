3319ec0e372e6a0e867ed9785ce6d57ed60c47c8
uiliam11 <100007488.joan23@fje.edu>
2020-12-16 00:12:25 +0100

    subida 16/12

--- js/script.js
+++ js/script.js
@@ -1,26 +1,80 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// Carousel
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel__item');
+const totalSlides = slides.length;
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+document.
+getElementById('carousel__button--next')
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    });
+document.
+getElementById('carousel__button--prev')
+    .addEventListener("click", function() {
+        moveToPrevSlide();
+    });
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel__item--visible');
+        slide.classList.add('carousel__item--hidden');
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+
+    slides[slidePosition].classList.add('carousel__item--visible');
 }
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+}
+
+// Countdown
+
+function countdown() {
+    var now = new Date();
+    var eventDate = new Date(2021, 5, 20);
+
+    var currentTiime = now.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTiime;
+
+    var s = Math.floor(remTime / 1000);
+    var m = Math.floor(s / 60);
+    var h = Math.floor(m / 60);
+    var d = Math.floor(h / 24);
+
+    h %= 24;
+    m %= 60;
+    s %= 60;
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    document.getElementById("days").textContent = d;
+    document.getElementById("days").innerText = d;
+
+    document.getElementById("hours").textContent = h;
+    document.getElementById("minutes").textContent = m;
+    document.getElementById("seconds").textContent = s;
+
+    setTimeout(countdown, 1000);
+}
+
+countdown();
